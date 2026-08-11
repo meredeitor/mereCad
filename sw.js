61e28107e64d95d9cac2a24cd83f490f31abffa8
Hermenegildo Perez Macias <gerente.sgi@caborca.biz>
2026-08-11 10:30:59 -0600

    Corregir instalación PWA y agregar iconos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='merecad-v2.2';
-const ASSETS=['./','./index.html','./styles.css','./cad-controls.css','./cad-ribbon.css','./cad-ribbon-extra.css','./compact-layout.css','./cad-navigation.css','./shape-library.css','./floor-symbols.css','./firebase-ui.css','./firebase-init.js','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+const CACHE='merecad-v2.3';
+const ASSETS=['./','./index.html','./styles.css','./cad-controls.css','./cad-ribbon.css','./cad-ribbon-extra.css','./compact-layout.css','./cad-navigation.css','./shape-library.css','./floor-symbols.css','./firebase-ui.css','./firebase-init.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
