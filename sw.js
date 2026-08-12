0aa27ffc53e90cdef6ee04b1064d399babfce88a
Hermenegildo Perez Macias <gerente.sgi@caborca.biz>
2026-08-12 08:35:17 -0600

    Mejorar vehiculos arquitectonicos en planta

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='merecad::';
-const CACHE=`${CACHE_PREFIX}v2.6`;
+const CACHE=`${CACHE_PREFIX}v2.7`;
 const APP_BASE='/mereCad/';
 const ASSETS=['./','./index.html','./styles.css','./cad-controls.css','./cad-ribbon.css','./cad-ribbon-extra.css','./compact-layout.css','./cad-navigation.css','./shape-library.css','./floor-symbols.css','./firebase-ui.css','./firebase-init.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 
